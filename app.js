6101ae45d0df5cfd4c90c6fadb4f371eac13adbb
ashir-valjee <ashir@eco-motive.com>
2025-02-20 23:26:35 +0000

    added js

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("hello");
+const submitButton = document.getElementById("submit-button");
+const dismissButton = document.getElementById("dismiss-button");
+
+const emailInput = document.getElementById("email");
+const emailSpan = document.querySelector(".email-span");
+
+const newsletterElement = document.querySelector(".newsletter-cta.hide");
+const successMessage = document.querySelector(".success_message");
+
+function handleSubmit() {
+  newsletterElement.style.display = "none";
+  successMessage.style.display = "grid";
+  emailSpan.textContent = emailInput.value;
+}
+function handleDismiss() {
+  successMessage.style.display = "none";
+  if (window.innerWidth > 650) {
+    newsletterElement.style.display = "grid";
+  } else {
+    newsletterElement.style.display = "block";
+  }
+}
+
+submitButton.addEventListener("click", handleSubmit);
+dismissButton.addEventListener("click", handleDismiss);
